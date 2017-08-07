52a5be67e2200e96321c84dfc62c0bcd911d023b
Tim Galloway <gallowaytim70@gmail.com>
2017-08-07 14:18:15 +0800

    Got the email going out

---
+++ DefectRecording/Helpers/Defects.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace DefectRecording.Helpers
+{
+    [Table("Defects")]
+    public class Defect
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        [MaxLength(500)]
+        public string ImgName { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+    }
+}
--- DefectRecording/MainActivity.cs
+++ DefectRecording/MainActivity.cs
@@ -8,6 +8,9 @@ using Android.Provider;
 using System.Collections.Generic;
 using Android.Content.PM;
 using SQLite;
+using MimeKit;
+using System.IO;
+using DefectRecording.Helpers;
 
 namespace DefectRecording
 {
@@ -31,15 +34,15 @@ namespace DefectRecording
                 button.Click += TakeAPicture;
 
                 Button button1 = FindViewById<Button>(Resource.Id.button1);
-                button1.Click += SavetoDB;
+                button1.Click += sendEmail;
 
             }
             var docFolder1 = "/sdcard/Android/data/DefectRecording/";
             var docFolder2 = "files/";
             var docsFolder = docFolder1 + docFolder2;
-            CreateDirectoryForDatabase(docFolder1,docFolder2);
+            CreateDirectoryForDatabase(docFolder1, docFolder2);
 
-            var pathToDatabase = System.IO.Path.Combine(docsFolder, "db_adonet.db");
+            var pathToDatabase = System.IO.Path.Combine(docsFolder, "DefectRecording.db");
             db = new SQLiteConnection(pathToDatabase);
             db.CreateTable<Defect>();
 
@@ -94,7 +97,7 @@ namespace DefectRecording
             //App._dir = new Java.IO.File(
             //    Android.OS.Environment.GetExternalStoragePublicDirectory(
             //        Android.OS.Environment.DirectoryPictures), "CameraAppDemo");
-            App._dir = new Java.IO.File(file1,file2);
+            App._dir = new Java.IO.File(file1, file2);
             if (!App._dir.Exists())
             {
                 App._dir.Mkdirs();
@@ -103,7 +106,7 @@ namespace DefectRecording
 
         private void CreateDirectoryForDatabase(String file1, String file2)
         {
-            Java.IO.File ldir = new Java.IO.File(file1,file2);
+            Java.IO.File ldir = new Java.IO.File(file1, file2);
             if (!ldir.Exists())
             {
                 ldir.Mkdirs();
@@ -124,20 +127,64 @@ namespace DefectRecording
             intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(App._file));
             StartActivityForResult(intent, 0);
         }
-    }
 
-    [Table("Defects")]
-    public class Defect
-    {
-        [PrimaryKey, AutoIncrement, Column("_id")]
-        public int Id { get; set; }
+        private void sendEmail(object sender, EventArgs ea)
+        {
+            try
+            {
+                EditText editText1 = FindViewById<EditText>(Resource.Id.editText1);
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Tim - Android", "galloway@iinet.net.au"));
+                message.To.Add(new MailboxAddress("Tim", "galloway@iinet.net.au"));
+                message.Subject = "How you doin?";
 
-        [MaxLength(500)]
-        public string ImgName { get; set; }
+                var body = new TextPart("plain")
+                {
+                    Text = editText1.Text
+                };
 
-        [MaxLength(500)]
-        public string Description { get; set; }
+                // create an image attachment for the file located at path
+                var attachment = new MimePart("image", "gif")
+                {
+                    ContentObject = new ContentObject(File.OpenRead(App._file.ToString()), ContentEncoding.Default),
+                    ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                    ContentTransferEncoding = ContentEncoding.Base64,
+                    FileName = System.IO.Path.GetFileName(App._file.ToString())
+                };
+
+                // now create the multipart/mixed container to hold the message text and the
+                // image attachment
+                var multipart = new Multipart("mixed");
+                multipart.Add(body);
+                multipart.Add(attachment);
+
+                // now set the multipart/mixed as the message body
+                message.Body = multipart;
+
+                using (var client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                    client.Connect("mail.iinet.net.au", 25, false);
+
+                    // Note: since we don't have an OAuth2 token, disable
+                    // the XOAUTH2 authentication mechanism.
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                    // Note: only needed if the SMTP server requires authentication
+                    client.Authenticate("galloway", "SuzTL1000");
+
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+                editText1.Text = "";
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
-
 
