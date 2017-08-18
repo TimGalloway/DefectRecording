6d49688cb4dec33d9004161da7f4cfa51d551602
Tim Galloway <gallowaytim70@gmail.com>
2017-08-18 15:53:52 +0800

    EOD

--- DefectRecording/Resources/Resource.Designer.cs
+++ DefectRecording/Resources/Resource.Designer.cs
@@ -75,8 +75,14 @@ namespace DefectRecording
 			// aapt resource value: 0x7f050000
 			public const int LocationLabel = 2131034112;
 			
+			// aapt resource value: 0x7f050005
+			public const int btnOpenCamera = 2131034117;
+			
 			// aapt resource value: 0x7f050004
 			public const int button1 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int txtLocation = 2131034118;
 			
 			static Id()
 			{
